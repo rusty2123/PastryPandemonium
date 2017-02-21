108cf27b3b8872369e64b6897e6249f8ac35dda7
Russell Chase <russellmchase@gmail.com>
2017-02-21 15:59:54 -0600

    added room name text

--- Pastry Pandemonium/Assets/Scripts/Networking/ListController.cs
+++ Pastry Pandemonium/Assets/Scripts/Networking/ListController.cs
@@ -1,16 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ListController : MonoBehaviour {
+public class ListController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject ListItemPrefab;
+    private RoomInfo[] roomsList;
+    public int yOffset = 10;
+
+
+    private void Update()
+    {
+        onRefresh();
+    }
+
+
+    public void onRefresh()
+    {
+        for (int i = 0; i < roomsList.Length; ++i)
+        {
+            GameObject newMultiplayerRoom = Instantiate(ListItemPrefab) as GameObject;
+            ListItemController controller = newMultiplayerRoom.GetComponent(typeof(ListItemController)) as ListItemController;
+            controller.name.text = roomsList[i].Name;
+            newMultiplayerRoom.name = roomsList[i].Name;
+            Debug.Log(controller.name.text);
+            Debug.Log(newMultiplayerRoom.name);
+        }
+    }
+
+
+
+    //public GameObject ContentPanel;
+    //public GameObject ListItemPrefab;
+
+    //private RoomInfo[] roomsList;
+
+    //void Start()
+    //{
+    //    if (roomsList != null)
+    //    {
+    //        for (int i = 0; i < roomsList.Length; ++i)
+    //        {
+    //            GameObject newMultiplayerRoom = Instantiate(ListItemPrefab) as GameObject;
+    //            ListItemController controller = newMultiplayerRoom.GetComponent(typeof(ListItemController)) as ListItemController;
+    //            controller.name.text = roomsList[i].Name;
+    //            newMultiplayerRoom.transform.parent = ContentPanel.transform;
+    //            newMultiplayerRoom.transform.localScale = Vector3.one;
+    //        }
+    //    }
+    //}
 }
--- Pastry Pandemonium/Assets/Scripts/Networking/Lobby.cs
+++ Pastry Pandemonium/Assets/Scripts/Networking/Lobby.cs
@@ -7,7 +7,7 @@ public class Lobby : Photon.PunBehaviour
 {
     #region Public Variables
 
-    public GameObject roomPanel, roomListItemPrefab;
+    public Text roomText;
 
     #endregion
 
@@ -79,6 +79,7 @@ public class Lobby : Photon.PunBehaviour
 
         if (PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default))
         {
+            roomText.text = roomText.text + "\n" + roomName;
             Debug.Log(roomName + " created");
         }
         else
@@ -93,8 +94,6 @@ public class Lobby : Photon.PunBehaviour
         {
             for (int i = 0; i < roomsList.Length; ++i)
             {
-                GameObject newRoom = Instantiate(roomListItemPrefab) as GameObject;
-
                 if (GUI.Button(new Rect(100, 250 + (110 * i), 160, 30), "Join " + roomsList[i].Name))
                 {
                     Join(roomsList[i].Name);
