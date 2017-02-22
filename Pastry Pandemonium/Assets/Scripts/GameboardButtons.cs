0d06b1dc4a442d8855579f3e7854c252f88f77be
Paul Toillion <ptoillion@harding.edu>
2017-02-21 19:42:15 -0600

    Added temporary exit and main menu buttons to game board

---
+++ Pastry Pandemonium/Assets/Scripts/GameboardButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameboardButtons : MonoBehaviour {
+
+	public GameObject current;
+
+
+
+	public void OnMouseUp()
+	{
+		if  (current.name == "exitButton") {
+				Application.Quit ();
+				Debug.Log ("quit application");
+			} 
+
+		if (current.name == "menuButton") {
+			SceneManager.LoadScene ("mainMenu");
+		}
+
+	}
+}
