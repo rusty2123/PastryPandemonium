1508043b6120e269df58b46604ca9c4f62af5afc
Paul Toillion <ptoillion@harding.edu>
2017-02-21 19:20:53 -0600

    Can go through tutorial

--- Pastry Pandemonium/Assets/Scripts/Tutorial.cs
+++ Pastry Pandemonium/Assets/Scripts/Tutorial.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour {
 
 	public GameObject current;
-	private GameObject menu, singlePlayer, multiplayer, tutorial, options, help, exit;
+	private GameObject menu, singlePlayer, multiplayer, tutorial, 
+					   options, help, exit, info, mills, placement, 
+	    		 	   moving, flying, backArrow, forwardArrow;
 
+	public static int position;
 
 	private void Awake()
 	{
@@ -17,11 +20,28 @@ public class Tutorial : MonoBehaviour {
 		singlePlayer = GameObject.FindGameObjectWithTag("singleplayer");
 		help = GameObject.FindGameObjectWithTag("help");
 		exit = GameObject.FindGameObjectWithTag("exit");
+
+		position = 1;
+
+		info = GameObject.Find("generalInfo");
+		mills = GameObject.Find("mills");
+		placement = GameObject.Find("placementPhase");
+		moving = GameObject.Find("movingPhase");
+		flying = GameObject.Find("flyingPhase");
+		backArrow = GameObject.Find("backArrow");
+		forwardArrow = GameObject.Find("forwardArrow");
+
+		backArrow.GetComponent<Renderer> ().enabled = false;
+		mills.GetComponent<Renderer> ().enabled = false;
+		placement.GetComponent<Renderer> ().enabled = false;
+		moving.GetComponent<Renderer> ().enabled = false;
+		flying.GetComponent<Renderer> ().enabled = false;
+
 	}
 	public void OnMouseEnter()
 	{
 		//Scales objects to indicate you can click on them
-		LeanTween.scale(current, new Vector3(0.5f, .5f, .5f), .075f);
+		LeanTween.scale(current, new Vector3(0.43f, .43f, .43f), .075f);
 
 	}
 
@@ -29,13 +49,47 @@ public class Tutorial : MonoBehaviour {
 	public void OnMouseExit()
 	{
 		//Sets objects back to their original size
-		LeanTween.scale(current, new Vector3(0.4268945f, 0.4268945f, 0.4268945f), .05f);
-
-
+		LeanTween.scale(current, new Vector3(0.37f, 0.37f, 0.37f), .05f);
 
 	}
 
+	private void setTutorial(int position)
+	{
+		switch (position) {
 
+		case 1:
+			backArrow.GetComponent<Renderer> ().enabled = false;
+			info.GetComponent<Renderer> ().enabled = true;
+			mills.GetComponent<Renderer> ().enabled = false;
+			break;
+		case 2:
+			backArrow.GetComponent<Renderer> ().enabled = true;
+			info.GetComponent<Renderer> ().enabled = false;
+			mills.GetComponent<Renderer> ().enabled = true;
+			placement.GetComponent<Renderer> ().enabled = false;
+			break;
+		case 3:
+			placement.GetComponent<Renderer> ().enabled = true;
+			mills.GetComponent<Renderer> ().enabled = false;
+			moving.GetComponent<Renderer> ().enabled = false;
+			break;
+		case 4:
+			placement.GetComponent<Renderer> ().enabled = false;
+			moving.GetComponent<Renderer> ().enabled = true;
+			flying.GetComponent<Renderer> ().enabled = false;
+			forwardArrow.GetComponent<Renderer> ().enabled = true;
+			break;
+		case 5:
+			moving.GetComponent<Renderer> ().enabled = false;
+			flying.GetComponent<Renderer> ().enabled = true;
+			forwardArrow.GetComponent<Renderer> ().enabled = false;
+			break;
+		default:
+			break;
+
+		}
+
+	}
 
 	public void OnMouseUp()
 	{
@@ -52,6 +106,19 @@ public class Tutorial : MonoBehaviour {
 				singlePlayer.SetActive (true);
 				help.SetActive (true);
 				exit.SetActive (true);
+				break;
+			case "forwardArrow":
+				if (position < 5) {
+					++position;
+				}
+				setTutorial (position);
+
+				break;
+			case "backArrow":
+				if (position > 1) {
+					--position;
+				}
+				setTutorial (position);
 				break;
 			default:
 				break;          
