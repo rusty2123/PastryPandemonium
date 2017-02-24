e3f2081e964bd9e252817599dd6f98203446f261
Paul Toillion <ptoillion@harding.edu>
2017-02-23 22:52:33 -0600

    Visuals for tutorial added

--- Pastry Pandemonium/Assets/Scripts/Tutorial.cs
+++ Pastry Pandemonium/Assets/Scripts/Tutorial.cs
@@ -7,7 +7,8 @@ public class Tutorial : MonoBehaviour {
 	public GameObject current;
 	private GameObject menu, singlePlayer, multiplayer, tutorial, 
 					   options, help, exit, info, mills, placement, 
-	    		 	   moving, flying, backArrow, forwardArrow;
+	    		 	   moving, flying, backArrow, forwardArrow,
+						phaseOne, phaseTwo, phaseThree;
 
 	public static int position;
 
@@ -31,17 +32,28 @@ public class Tutorial : MonoBehaviour {
 		backArrow = GameObject.Find("backArrow");
 		forwardArrow = GameObject.Find("forwardArrow");
 
+		phaseOne = GameObject.Find("phaseOne");
+		phaseTwo = GameObject.Find("phaseTwo");
+		phaseThree = GameObject.Find("phaseThree");
+
 		backArrow.GetComponent<Renderer> ().enabled = false;
 		mills.GetComponent<Renderer> ().enabled = false;
 		placement.GetComponent<Renderer> ().enabled = false;
 		moving.GetComponent<Renderer> ().enabled = false;
 		flying.GetComponent<Renderer> ().enabled = false;
+		phaseOne.GetComponent<Renderer> ().enabled = false;
+		phaseTwo.GetComponent<Renderer> ().enabled = false;
+		phaseThree.GetComponent<Renderer> ().enabled = false;
 
 	}
 	public void OnMouseEnter()
 	{
 		//Scales objects to indicate you can click on them
-		LeanTween.scale(current, new Vector3(0.43f, .43f, .43f), .075f);
+		if (current.name == "menuButton") {
+			LeanTween.scale (current, new Vector3 (0.43f, .43f, .43f), .075f);
+		} else {
+			LeanTween.scale (current, new Vector3 (0.3f, .3f, .3f), .075f);
+		}
 
 	}
 
@@ -49,7 +61,13 @@ public class Tutorial : MonoBehaviour {
 	public void OnMouseExit()
 	{
 		//Sets objects back to their original size
-		LeanTween.scale(current, new Vector3(0.37f, 0.37f, 0.37f), .05f);
+		if (current.name == "menuButton") 
+		{
+			LeanTween.scale (current, new Vector3 (0.37f, 0.37f, 0.37f), .05f);
+		}
+		else {
+			LeanTween.scale (current, new Vector3 (0.2023873f, .2023873f, .2023873f), .075f);
+		}
 
 	}
 
@@ -67,21 +85,29 @@ public class Tutorial : MonoBehaviour {
 			info.GetComponent<Renderer> ().enabled = false;
 			mills.GetComponent<Renderer> ().enabled = true;
 			placement.GetComponent<Renderer> ().enabled = false;
+			phaseOne.GetComponent<Renderer> ().enabled = false;
 			break;
 		case 3:
 			placement.GetComponent<Renderer> ().enabled = true;
 			mills.GetComponent<Renderer> ().enabled = false;
 			moving.GetComponent<Renderer> ().enabled = false;
+			phaseTwo.GetComponent<Renderer> ().enabled = false;
+			phaseOne.GetComponent<Renderer> ().enabled = true;
 			break;
 		case 4:
 			placement.GetComponent<Renderer> ().enabled = false;
+			phaseOne.GetComponent<Renderer> ().enabled = false;
 			moving.GetComponent<Renderer> ().enabled = true;
 			flying.GetComponent<Renderer> ().enabled = false;
 			forwardArrow.GetComponent<Renderer> ().enabled = true;
+			phaseThree.GetComponent<Renderer> ().enabled = false;
+			phaseTwo.GetComponent<Renderer> ().enabled = true;
 			break;
 		case 5:
 			moving.GetComponent<Renderer> ().enabled = false;
+			phaseTwo.GetComponent<Renderer> ().enabled = false;
 			flying.GetComponent<Renderer> ().enabled = true;
+			phaseThree.GetComponent<Renderer> ().enabled = true;
 			forwardArrow.GetComponent<Renderer> ().enabled = false;
 			break;
 		default:
