93d693a8cabe0952972fdbf0133bb562e3bcdaae
Paul Toillion <ptoillion@harding.edu>
2017-03-17 14:31:07 -0500

    Save Preferences

---
+++ Pastry Pandemonium/Assets/Scripts/Music.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Music : MonoBehaviour {
+
+	static bool AudioBegin = false; 
+	public Slider vol; 
+	private AudioSource audioSource;
+	//public AudioClip audio;
+
+	void Awake()
+	{
+		audioSource = GetComponent<AudioSource> ();
+
+		if (!AudioBegin) {
+
+
+			audioSource.Play ();
+			DontDestroyOnLoad (audioSource);
+
+			AudioBegin = true;
+		}
+	}
+
+
+	public void changeVolume()
+	{
+		audioSource.volume = vol.value;
+	}
+}
---
+++ Pastry Pandemonium/Assets/Scripts/Preferences.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Preferences : MonoBehaviour {
+
+
+	public Slider musicVolSlider;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		musicVolSlider.value = PlayerPrefs.GetFloat ("musicSlider");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		PlayerPrefs.SetFloat ("musicSlider", musicVolSlider.value);
+
+	}
+}
