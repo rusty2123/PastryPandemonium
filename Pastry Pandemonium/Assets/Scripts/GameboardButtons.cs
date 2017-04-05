f90e241d1f28363bb1a73e339a7b02d54ef78485
Paul Toillion <ptoillion@harding.edu>
2017-04-05 11:20:14 -0500

    New hints and mute buttons

--- Pastry Pandemonium/Assets/Scripts/GameboardButtons.cs
+++ Pastry Pandemonium/Assets/Scripts/GameboardButtons.cs
@@ -7,6 +7,49 @@ public class GameboardButtons : MonoBehaviour {
 
     public GameObject current;
     public NetworkGameManager networkManager;
+	private GameObject musicSwitchRight, musicSwitchLeft, switchMusic, onSwitchMusic, offSwitchMusic;
+	private GameObject hintsSwitchRight , hintsSwitchLeft, switchHints, onSwitchHints, offSwitchHints;
+	private bool hints = true;
+
+
+	private void Awake()
+	{
+
+		musicSwitchRight = GameObject.Find ("musicSwitchRight");
+		musicSwitchLeft = GameObject.Find ("musicSwitchLeft");
+		switchMusic = GameObject.Find ("switchMusic");
+		onSwitchMusic = GameObject.Find ("onSwitchMusic");
+		offSwitchMusic = GameObject.Find ("offSwitchMusic");
+
+
+		hintsSwitchRight = GameObject.Find ("hintsSwitchRight");
+		hintsSwitchLeft = GameObject.Find ("hintsSwitchLeft");
+		switchHints = GameObject.Find ("switchHints");
+		onSwitchHints = GameObject.Find ("onSwitchHints");
+		offSwitchHints = GameObject.Find ("offSwitchHints");
+
+
+		onSwitchHints.GetComponent<Renderer> ().enabled = true;
+		offSwitchHints.GetComponent<Renderer> ().enabled = false;
+		switchHints.transform.position = hintsSwitchRight.transform.position;
+
+
+		Music musicInstance = Music.getInstance ();
+
+		if (musicInstance.vol.value == 0f) {
+			onSwitchMusic.GetComponent<Renderer> ().enabled = false;
+			offSwitchMusic.GetComponent<Renderer> ().enabled = true;
+			switchMusic.transform.position = musicSwitchLeft.transform.position;
+		} else {
+
+			onSwitchMusic.GetComponent<Renderer> ().enabled = true;
+			offSwitchMusic.GetComponent<Renderer> ().enabled = false;
+			switchMusic.transform.position = musicSwitchRight.transform.position;
+
+		}
+
+
+	}
 
 	public void OnMouseUp()
 	{
@@ -19,18 +62,38 @@ public class GameboardButtons : MonoBehaviour {
 				}
 				SceneManager.LoadScene ("mainMenu");
 			break;
-		case "mute":
+		case "musicMute":
 				// turn off the music here
 			Music musicInstance = Music.getInstance ();
 			if (musicInstance.vol.value == 0f) {
 				musicInstance.vol.value = musicInstance.getLastVolume ();
+				onSwitchMusic.GetComponent<Renderer> ().enabled = true;
+				offSwitchMusic.GetComponent<Renderer> ().enabled = false;
+				switchMusic.transform.position = musicSwitchRight.transform.position;
 			} else {
 				musicInstance.setLastVolume (musicInstance.vol.value);
 				musicInstance.vol.value = 0f;
+				onSwitchMusic.GetComponent<Renderer> ().enabled = false;
+				offSwitchMusic.GetComponent<Renderer> ().enabled = true;
+				switchMusic.transform.position = musicSwitchLeft.transform.position;
+
+
 			}
 			break;
-		case "helpfulHints":
-			App.showHelfulHints = !App.showHelfulHints;
+		case "hints":
+			if (hints) {
+				App.showHelfulHints = !App.showHelfulHints;
+				onSwitchHints.GetComponent<Renderer> ().enabled = false;
+				offSwitchHints.GetComponent<Renderer> ().enabled = true;
+				switchHints.transform.position = hintsSwitchLeft.transform.position;
+				hints = false;
+			} else {
+				App.showHelfulHints = true;
+				onSwitchHints.GetComponent<Renderer> ().enabled = true;
+				offSwitchHints.GetComponent<Renderer> ().enabled = false;
+				switchHints.transform.position = hintsSwitchRight.transform.position;
+				hints = true;
+			}
 			break;
 		default:
 			break;
